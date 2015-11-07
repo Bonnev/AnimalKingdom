65000e5cd9ea3906512f32561a3cf5953da49398
Bonnev <piggov@gmail.com>
2015-11-07 21:58:20 +0200

    Added a map (by Google) to the AddAnimal page

--- AnimalKingdom/AnimalKingdom.Server/Controllers/AnimalController.cs
+++ AnimalKingdom/AnimalKingdom.Server/Controllers/AnimalController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 
 namespace AnimalKingdom.Server.Controllers
 {
+    using System.Data.Entity.Spatial;
+
     using AnimalKingdom.Data;
 
     public class AnimalController : Controller
@@ -19,6 +21,18 @@ namespace AnimalKingdom.Server.Controllers
         public ActionResult ViewAnimals()
         {
             return View();
+        }
+
+        
+        public string Evaluate(string coordinates)
+        {
+            //string id, string dataToEvaluate)
+            //'idHideen', dataToEvaluate: event.latLng
+            string latitude = coordinates.Substring(1, coordinates.IndexOf(' ') - 2);
+            string longitude = coordinates.Substring(coordinates.IndexOf(' ') + 1, coordinates.Length - coordinates.IndexOf(' ') - 2);
+            string pointString = string.Format("POINT({0} {1})", longitude, latitude);
+            var geo = DbGeography.FromText(pointString);
+            return pointString;
         }
     }
 }
