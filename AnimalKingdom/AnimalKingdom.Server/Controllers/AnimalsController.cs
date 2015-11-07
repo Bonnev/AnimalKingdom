3052e725526a25c97356dcd9984378d38623ecaf
Venciity <venciity@abv.bg>
2015-11-07 23:12:07 +0200

    Added some missing files

---
+++ AnimalKingdom/AnimalKingdom.Server/Controllers/AnimalsController.cs
@@ -0,0 +1,36 @@
+﻿namespace AnimalKingdom.Server.Controllers
+{
+    using System.Web.Mvc;
+    using Data.UnitOfWork;
+    using Models.BindingModels;
+    using System.Linq;
+
+    public class AnimalsController : BaseController
+    {
+        public AnimalsController(IAnimalKingdomData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Add()
+        {
+            ViewBag.Genders = this.Data.Genders.All()
+                .Select(g => new SelectListItem() { Text = g.Name, Value = g.Id.ToString() });
+            ViewBag.Types = this.Data.AnimalTypes.All()
+                .Select(t => new SelectListItem() { Text = t.Name, Value = t.Id.ToString() });
+            // TODO: Connect to 1st select list
+            ViewBag.Breeds = this.Data.Breeds.All()
+                .Select(b => new SelectListItem() { Text = b.Name, Value = b.Id.ToString() });
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize, ValidateAntiForgeryToken]
+        public ActionResult Add(AnimalBindingModel model)
+        {
+            return this.View();
+        }
+    }
+}
---
+++ AnimalKingdom/AnimalKingdom.Server/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using AnimalKingdom.Data.UnitOfWork;
+using AnimalKingdom.Server.Models.ViewModels;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalKingdom.Server.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public UsersController(IAnimalKingdomData data)
+            : base(data)
+        {
+        }
+
+        [HttpPost]
+        public ActionResult All(FilterViewModel filter)
+        {
+            var users = this.Data.Users.All();
+            foreach (var filterModel in filter.Filters)
+            {
+                if (filterModel.Operator == "contains")
+                {
+                    users = users.Where(u => u.UserName.ToLower().Contains(filterModel.Value.ToLower()));
+                }
+                else if (filterModel.Operator == "neq")
+                {
+                    users = users.Where(u => u.UserName != filterModel.Value);
+                }
+            }
+
+            var model = Mapper.Map<IEnumerable<UserConciseViewModel>>(users);
+            return this.Json(model);
+        }
+    }
+}
