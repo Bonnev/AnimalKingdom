957c773761dfe73243c5fb5d3494e2148a503197
Bonnev <piggov@gmail.com>
2015-11-07 17:22:35 +0200

    Some forms added

---
+++ AnimalKingdom/AnimalKingdom.Server/Controllers/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalKingdom.Server.Controllers
+{
+    using AnimalKingdom.Data;
+
+    public class AnimalController : Controller
+    {
+        // GET: Animal
+        public ActionResult AddAnimal()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAnimals()
+        {
+            return View();
+        }
+    }
+}
